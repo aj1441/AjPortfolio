5b0ae430d7e16b5fefa8b485e2b03457410003eb
aj1441 <167340937+aj1441@users.noreply.github.com>
2024-07-24 12:58:48 -0700

    functions are done working on final styling

--- js/jsfunfunctions.js
+++ js/jsfunfunctions.js
@@ -8,81 +8,103 @@ const burgerButton= document.querySelector("button.burger");
 function showHamburger() {
   burger.classList.toggle("fa-times");
   nav.classList.toggle("nav-active"); 
+// document.getElementById("pageBase").style.opacity = "0.5";
+  
 }
 
 // eventListeners-------------------------------------------------------------------------------------
 burgerButton.addEventListener("click", showHamburger);
 
 
-//jsfun page functions--------------------------------------------
-//js fun variable declaration------------------------------------------------------------------------------
-const colorFormButton = document.getElementById("js-colors-btn");
-const colorForm = document.getElementById("color-fun-form");
-const nycTime = document.getElementById("js-time-btn");
-const displayNycTime = document.getElementById("show-nyc-time");
-const madLib = document.getElementById("js-madlib-btn");
-const madLibForm = document.getElementById("madlib-form");
 
-//js fun event listeners------------------------------------------------------------------------------------
-colorFormButton.addEventListener("click", toggleColorForm);
+
+//js nyc time ----------------------------------------------------------------------------------------------
+const nycTime = document.getElementById("jsTimeBtn");
+const displayNycTime = document.getElementById("nycTime");
 nycTime.addEventListener("click", toggleNycTime);
-madLib.addEventListener("click", toggleMadLib)
 
-
-
-//js fun toggle----------------------------------------------------------------------------------------------
-function toggleColorForm() {
-    document.getElementById("colors-fun-wrapper").classList.toggle("js-hide-form");
-  }
 function toggleNycTime(){
     document.getElementById("nyc-time").classList.toggle("js-hide-form");
     getNycTime();
 }
-function toggleMadLib(){
-    document.getElementById("madlib-wrapper").classList.toggle("js-hide-form");
-}
-
-
-//jsfun colors function-------------------------------------------------------------------------------------
-function changeColor(colorFromUser, type) {
-    console.log(colorFromUser, type)
-    if(type === "background"){
-      colorForm.style.backgroundColor = colorFromUser;
-    } else {
-      colorForm.style.color = colorFromUser;
-    }
-    // const color = document.getElementById("color").value;
-  // colorForm.style.backgroundColor = colorFromUser;
-    
-  }
-  
-  //js fun colors submit functions
-  function onColorFormSubmit(event) {
-    console.log("color form submitted");
-      event.preventDefault();
-      const data = new FormData(event.target);
-      const dataObject = Object.fromEntries(data.entries());
-      console.log(dataObject);
-      colorForm.reset();
-      //above is boiler plate code
-      changeColor(dataObject.colorSelect, dataObject.typeSelect)
-  }
-
-//js fun nyc time--------------------------------------------------------------------
 function getNycTime(){
     const currentDate = new Date();
     let nycTimeNow = currentDate.toLocaleTimeString('en-US', {timeZone: "America/New_York"})
     displayNycTime.textContent = `The current time in New York City is ${nycTimeNow}`;
     console.log(currentDate);
     }
+//------------------------------------------------------------------------------------------
 
 
+//jsfun colors function-------------------------------------------------------------------------------------
 
-  
+const colorFormButton = document.getElementById("jsColorsBtn");
+const colorForm = document.getElementById("colorForm");
+const colorSubmit = document.getElementById("colorFormSubmit");
 
-// 
-//jsfun madlib function--------------------------------------------------------------
-//   function makeAMadLib(noun, verb, adjective, adverb) {
-// 	let response = `You are ${dataObject.verb}! Your favorite thing is  ${dataObject.noun}! You love to ${dataObject.verb} ${dataObject.adverb}. Our class was the ${adjectiveThree} class that our teachers had ever had.`;
-// 	para.textContent = response;
-// 	// return response;
+colorFormButton.addEventListener("click", toggleColorForm);
+colorForm.addEventListener("submit", onColorFormSubmit);
+
+function toggleColorForm() {
+    document.getElementById("colorsFunWrapper").classList.toggle("js-hide-form");
+    
+  }
+
+  function onColorFormSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const dataObject = Object.fromEntries(data.entries());
+        console.log(dataObject);
+      colorForm.reset();
+      //above is boiler plate code
+      let userType = dataObject.chooseType;
+      let userColor = dataObject.chooseColor;
+      console.log(userType);
+      console.log(userColor);
+   
+      changeColor(userColor, userType);
+  }
+
+  function changeColor(colorFromUser, type) {
+    if(type === "background"){
+      colorForm.style.backgroundColor = colorFromUser;
+    } else {
+      colorForm.style.color = colorFromUser;
+    }
+  }
+//-------------------------------------------------------------------------------------------
+
+
+//js fun madlib submit function-------------------------------------------------------------
+const madlib = document.getElementById("jsMadlibBtn");
+const madlibForm = document.getElementById("madlibForm");
+const displayMadlib = document.getElementById("madlibSentence");
+const madlibSubmitBtn = document.getElementById("madlibSubmit");
+
+madlib.addEventListener("click", toggleMadlib);
+madlibForm.addEventListener("submit", onMadlibSubmit);
+
+
+function toggleMadlib(){
+    document.getElementById("madlibWrapperId").classList.toggle("js-hide-form");
+}
+
+
+function onMadlibSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const dataObject = Object.fromEntries(data.entries());
+        console.log(dataObject);
+      madlibForm.reset();
+      let adjective = dataObject.adjective;
+      let noun = dataObject.noun;
+      let adverb = dataObject.adverb;
+      let verb = dataObject.verb;
+      displayMadlib.textContent = `You are a ${adjective} ${noun}! You love to ${adverb} ${verb}!`;
+  }
+
+
+//  	    let response = `You are a ${dataObject.adjective} ${dataObject.noun}! You love to ${dataObject.adverb} ${dataObject.adverb}!`;
+//  	    para.textContent = response;
+//          return response;
+//   }
